e75ef503c083a9abfb6cb730dc09427b7b41ffa0
Frey Hargarten <harga012@umn.edu>
2020-10-08 11:57:39 -0500

    updates

--- src/MappersRow.svelte
+++ src/MappersRow.svelte
@@ -4,14 +4,15 @@ import county_results_history from './scraper/json/county-results-history.json';
 import IsoMap from './IsoMap.svelte'
 
 export let county_data_2020;
-export let current_week;
 
 </script>
 
 
+<p class="buffer">&nbsp;</p>
+
 <div id="map-row">
   <div class="map-group">
-    <h3>Absentee ballots accepted so far</h3>
+    <div class="chartTitle">Absentee ballots accepted so far</div>
     <div class="map-subrow">
       <div class="map-item">
         <h4>Trump 2016 counties</h4>
@@ -24,7 +25,7 @@ export let current_week;
     </div>
   </div>
   <div class="map-group">
-    <h3>Ballots accepted compared to {current_week} week{#if current_week != 1}s{/if} to go in 2018</h3>
+    <div class="chartTitle">Ballots accepted compared to 2018</div>
     <div class="map-subrow">
       <div class="map-item">
         <h4>Trump 2016 counties</h4>
